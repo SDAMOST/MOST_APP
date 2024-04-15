62939e6a2f89517075e94162611a848422e190ea
Jakub <kuba.staniszewski.ks@gmail.com>
2024-04-16 00:06:58 +0200

    create home screen

---
+++ src/screens/HomeScreen/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+function HomeScreen() {
+  return (
+    <Text>
+      To jest ekran główny
+    </Text>
+  )
+}
+
+export default HomeScreen;
