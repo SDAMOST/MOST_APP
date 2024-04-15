d3e8b180548325c952f2810a7b5d65c26d621b08
Jakub <kuba.staniszewski.ks@gmail.com>
2024-04-16 00:07:11 +0200

    create offer intention screen

---
+++ src/screens/OfferIntentionScreen/OfferIntentionScreen.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text } from 'react-native'
+
+function OfferIntentionScreen() {
+  return (
+    <Text>
+      To jest ekran do zamawiania intencji
+    </Text>
+  )
+}
+
+export default OfferIntentionScreen
